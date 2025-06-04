3f527dc9feac0190118846115561cf0392476905
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-04 15:09:30 +0900

    feat: Add App

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Pay from "./pages/Pay";
+import Order from "./pages/Order";
+import Other from "./pages/Other";
+import "./styles/Navigation.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Navigation />
+      <main className="page-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pay" element={<Pay />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/other" element={<Other />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
