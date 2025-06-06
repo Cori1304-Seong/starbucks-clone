0b3f1722df193842b6b242de9b0b2a5dac4e1328
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-06 16:28:33 +0900

    feat: Add Greeting component

---
+++ src/components/home/greeting/Greeting.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import { CommonBtn } from "../../common/customBtn";
+import { CommonText } from "../../common/customText";
+import { CommonBtnGroup } from "../../common/BtnGroup";
+
+function Greeting() {
+  const navigate = useNavigate();
+
+  return (
+    <section className="pt-5 pb-4 px-7 bg-[bg-app]">
+      <CommonText fontSize="text-xl" bold>
+        스타벅스에 오신것을 환영합니다!
+      </CommonText>
+
+      <CommonBtnGroup gap="gap-6" padding="px-7">
+        <CommonBtn
+          title="로그인"
+          bgColor="bg-white"
+          textColor="text-[#006241]"
+          hoverOpacity="hover:opacity-90"
+          fullWidth={true}
+          onClick={() => navigate("/login")}
+        />
+
+        <CommonBtn
+          title="회원가입"
+          bgColor="bg-[#006241]"
+          textColor="text-white"
+          hoverOpacity="hover:opacity-90"
+          fullWidth={true}
+          onClick={() => navigate("/signup")}
+        />
+      </CommonBtnGroup>
+    </section>
+  );
+}
+
+export default Greeting;
