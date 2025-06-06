256b4ebdb481e997dbb70041f0c0e6eae7651567
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-06 19:45:46 +0900

    feat: Add Login path

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Pay from "./pages/Pay";
 import Order from "./pages/Order";
 import Other from "./pages/Other";
+import Login from "./pages/Login";
 
 import "./App.css";
 
@@ -18,6 +19,8 @@ function App() {
           <Route path="/pay" element={<Pay />} />
           <Route path="/order" element={<Order />} />
           <Route path="/other" element={<Other />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </main>
     </div>
