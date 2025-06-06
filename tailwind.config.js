56d43f62e9a16dd3da207f72438ee76b933f6cbc
Cori1304-Seong <andyjm7739@gmail.com>
2025-06-06 16:31:00 +0900

    feat: Add Pretendard fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  extend: {
+    fontFamily: {
+      sans: ["Pretendard", "sans-serif"],
+    },
+    colors: {
+      "bg-app": "#f6f5ef", // 배경색
+    },
   },
   plugins: [],
 };
